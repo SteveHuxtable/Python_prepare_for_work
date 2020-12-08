eace19cf289e68bcf551c50a2570c88dbde82176
Steve Hu <stevehu@StevedeMacBook-Pro.local>
2020-12-08 21:41:31 +0800

    20201208

---
+++ my_math.py
@@ -0,0 +1,12 @@
+def square(x):
+    '''
+    calculate the square of a number
+    >>> square(2)
+    4
+    >>> square(3)
+    9
+    '''
+    return x * x
+
+def my_product(x, y):
+    return x * y
